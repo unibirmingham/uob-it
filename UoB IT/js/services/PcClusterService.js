c70545449132e5c123778d498b8844cf1162658a
withersc <withers@bham.ac.uk>
2015-11-04 16:27:41 +0000

    Started to flesh out Campus Service functionality

--- UoB IT/js/services/PcClusterService.js
+++ UoB IT/js/services/PcClusterService.js
@@ -10,9 +10,20 @@ app.registerInitialise(function () {
 
         var forceRefreshKeys = {};
 
-        var cacheKeys = { AllPCs: "%LOCATION_REPOSITORY_GET_ALL_PCS" };
+        var cacheKeys = {
+            AllPCs: "%PC_CLUSTER_SERVICE_GET_ALL_PCS",
+            Campus: "%PC_CLUSTER_SERVICE_CAMPUS%"
+        };
 
-        var urls = { nearestPc: "http://www.birmingham.ac.uk/web_services/Clusters.svc/nearestpc" };
+        // List of Service endpoints for Pc Cluster functions
+        // Some service requests require specific data such as buildingId or MapId
+        var urls = {
+            NearestPCs: "http://www.birmingham.ac.uk/web_services/Clusters.svc/nearestpc",
+            Campus: "http://www.birmingham.ac.uk/web_services/Clusters.svc/maps",
+            Buildings: "http://www.birmingham.ac.uk/web_services/Clusters.svc/maps/{MAPID}/buildings",
+            Clusters: "http://www.birmingham.ac.uk/web_services/Clusters.svc/buildings/{BUILDINGID}/clusters"
+
+        };
 
         //generates the service url for PC location based upon current or a default location
         var pcUrlCreator = function (coords) {
@@ -21,7 +32,7 @@ app.registerInitialise(function () {
                 coords = [52.45049111433046, -1.9307774305343628];
             }
 
-            return pcsUrl + "?lat=" + coords[0] + "&long=" + coords[1];
+            return urls.NearestPCs + "?lat=" + coords[0] + "&long=" + coords[1];
         }
 
 
@@ -41,6 +52,16 @@ app.registerInitialise(function () {
 
         });
 
+        var getCampuses = Promise.method(function() {
+            return new Promise(function(resolve, reject) {
+                return RemoteServiceManager.FetchRemoteCache(urls.Campus, cacheKeys.Campus).then(function (result) {
+                    resolve(result);
+                }).catch(function (error) {
+                    reject(error);
+                });
+            });
+        });
+
 
         //adds a key to the refresh array. On the next local storage request
         //for the specified key, a remote fetch will occur, the results
@@ -52,6 +73,7 @@ app.registerInitialise(function () {
 
 
         return {
+            GetCampuses: getCampuses,
             GetNearestPCs: getNearestPcsToMe,
             RefreshData: forceRefresh,
             CacheKeys: cacheKeys
